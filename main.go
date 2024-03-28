01af023021d4c01963c669cc28a37bb72ed72bed
Marcel Jaworski <marcel.jaworski@ils-gmbh.net>
2024-03-28 10:08:13 +0100

    find by ID

--- main.go
+++ main.go
@@ -5,11 +5,14 @@ import (
 	"net/http"
 )
 
+func findByID(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	w.Write([]byte("received request for item: " + id))
+}
 func main() {
 	router := http.NewServeMux()
-	router.HandleFunc("/item/{id}", func(w http.ResponseWriter, r *http.Request) {
-
-	})
+	// Path parameter id
+	router.HandleFunc("/item/{id}", findByID)
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: router,
